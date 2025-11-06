d852df995a09d928879bbf573606545161ddfaa8
Chizhikov <57985484+SergioChizhikov@users.noreply.github.com>
2025-11-06 13:12:03 +0300

    Ходит по неограниченному кол-ву сайтов

--- bot.js
+++ bot.js
@@ -6,6 +6,8 @@
 // @author       Sergey Chizhikov
 // @match        https://www.google.com/*
 // @match        https://napli.ru/*
+// @match        https://kiteuniverse.ru/*
+// @match        https://motoreforma.com/*
 // @grant        none
 // ==/UserScript==
 
@@ -15,14 +17,31 @@
   let input = document.getElementsByName("q")[0];
   let button = document.getElementsByName("btnK")[0];
   let links = document.links;
-  let keywords = [
-    "Базовые вещи про GIT. Настройка и основные команды.",
-    "вывод произвольных полей wordpress",
-    "Конвертация Notion в Obsidian", "10 самых популярных шрифтов от Google",
-    "Как правильно задавать вопросы",
-  ];
-  
-  let keyword = "Как правильно задавать вопросы";
+  let sites = {
+    "napli.ru": [
+      "Базовые вещи про GIT. Настройка и основные команды.",
+      "вывод произвольных полей wordpress",
+      "Конвертация Notion в Obsidian", "10 самых популярных шрифтов от Google",
+      "Как правильно задавать вопросы",
+    ],
+    "kiteuniverse.ru": ["Красота. Грация. Интеллект", "Kite Universe", "Мастер классы Kite Universe"],
+    "motoreforma.com": ["Мотореформа", "тюнинг maverick X3", "прошивки для CAN_AM"],
+  }
+  let site = Object.keys(sites)[getRandom(0, Object.keys(sites).length)];
+  let keywords = sites[site];
+
+  let keyword = keywords[getRandom(0, keywords.length)];
+
+  if (button !== undefined) {
+    document.cookie = `site=${site}`;
+  } else if (location.hostname == "www.google.com") {
+    site = getCookie("site");
+  } else {
+    site = location.hostname;
+  }
+
+
+
   if (button !== undefined) {
     let i = 0;
     let timerId = setInterval(() => {
@@ -33,14 +52,28 @@
         button.click();
       }
     }, 200);
-  } else if (location.hostname == "napli.ru") {
+  } else if (location.hostname == site) {
     console.log("Мы на целевом сайте");
+
+    setInterval(() => {
+      let index = getRandom(0, links.length);
+      let link = links[index];
+
+      if (getRandom(0, 101) >= 80) {
+        location.href = "https://www.google.com/";
+      }
+
+      if (link.href.includes(site)) {
+        link.click();
+      }
+
+    }, getRandom(2000, 4000))
   } else {
     //Работаем на странице выдачи
     let nextPage = true;
 
     for (let i = 0; i < links.length; i++) {
-      if (links[i].href.indexOf("napli.ru") != -1) {
+      if (links[i].href.indexOf(site) != -1) {
         let link = links[i];
         console.log("Нашел строку" + links[i]);
         setTimeout(() => {
@@ -65,4 +98,11 @@
   function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
   }
-})();
+  function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+     return matches ? decodeURIComponent(matches[1]) : undefined;
+  }
+
+  })();
