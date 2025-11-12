8c5bc5bb9aa705dde95eaa9df1e35c06e26447b2
Chizhikov <57985484+SergioChizhikov@users.noreply.github.com>
2025-11-12 12:49:14 +0300

    Update reg_obr.php

--- reg_obr.php
+++ reg_obr.php
@@ -2,16 +2,16 @@
 header('Content-Type: text/html; charset=utf-8');
 
 
+
 $mysqli = mysqli_connect($host, $user, $password, $db);
 
-if ($mysqli == false) {
-  print("Ошибка: Невозможно подключиться к MySQL ");
-} else {
-  //print("Соединение установлено успешно");
+
+if (!empty($_POST["name"]) && !empty($lastname = $_POST["lastname"])) {
   $name = $_POST["name"];
   $lastname = $_POST["lastname"];
-  $email = $_POST["email"];
-  $pass = $_POST["pass"];
+  $email = trim(mb_strtolower($_POST["email"]));
+  $pass = trim($_POST["pass"]);
+  $pass = password_hash($pass, PASSWORD_DEFAULT);
 
   $result = $mysqli->query("SELECT * FROM `users` WHERE `email`='$email'");
   // var_dump($result->num_rows);
@@ -21,7 +21,13 @@ if ($mysqli == false) {
   } else {
     print("ok");
     $mysqli->query("INSERT INTO `users`(`name`, `lastname`, `email`, `pass`) VALUES ('$name', '$lastname', '$email', '$pass')");
+    // mail('se@napli.ru', 'Тестовой письмо с приветом', "Привет, Сергей. Вот тебе данные: $name $lastname");
   }
+} else {
+  echo "empty";
+
+}
+
 
 
 
@@ -30,4 +36,3 @@ if ($mysqli == false) {
   // $stmt = $mysqli->prepare($sql);
   // $stmt->bind_param("ssss", $name, $lastname, $email, $pass);
   // $stmt->execute();
-}
